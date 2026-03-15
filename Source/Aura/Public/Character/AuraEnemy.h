af73790aa2a59bfe33face026d9905fc10b24aa5
Jacob <deathbeforedismountgame@gmail.com>
2026-03-15 01:17:10 -0400

    Enemy Progress bar + Ghost Bar + Visibility nodes

--- Source/Aura/Public/Character/AuraEnemy.h
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -5,8 +5,11 @@
 #include "CoreMinimal.h"
 #include "Character/AuraCharacterBase.h"
 #include "Interaction/EnemyInterface.h"
+#include "Ui/WIdgetController/OverlayWidgetController.h"
 #include "AuraEnemy.generated.h"
 
+
+class UWidgetComponent;
 /**
  * 
  */
@@ -27,11 +30,20 @@ public:
 	
 	// End Combat Interface
 	
+	UPROPERTY(BlueprintAssignable)
+	FOnAttributeChangedSignature OnHealthChanged;
+	
+	UPROPERTY(BlueprintAssignable)
+	FOnAttributeChangedSignature OnMaxHealthChanged;
+	
 protected:
 	virtual void BeginPlay() override;
 	virtual void InitAbilityActorInfo() override;
 	
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Character Class Defaults")
 	int32 Level = 1;
+	
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly)
+	TObjectPtr<UWidgetComponent> HealthBar;
 	
 };
